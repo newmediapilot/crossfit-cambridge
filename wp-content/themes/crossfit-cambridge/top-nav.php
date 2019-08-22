2b120598794775d7a60e183ca7eca57803cb63ff
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-08-21 21:49:39 -0400

    building nav

--- wp-content/themes/crossfit-cambridge/top-nav.php
+++ wp-content/themes/crossfit-cambridge/top-nav.php
@@ -1,21 +1,73 @@
-<ul class="nav nav-tabs">
-    <li class="nav-item">
-        <a class="nav-link active" href="#">Active</a>
-    </li>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Separated link</a>
+<nav class="npm-top-nav">
+    <div class="nmp-top-nav--top">
+        <div class="top--left">
+            Top left
         </div>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-    </li>
-</ul>
+        <div class="top--right">
+            Top right
+        </div>
+    </div>
+    <div class="nmp-top-nav--bottom">
+        <a href="https://goo.gl/maps/V4vqwnG93yv6W48cA"
+           title="Open Map (opens a new window)"
+           target="_blank" class="bottom--left">
+            <i class="fas fa-map-marker-alt"></i>&nbsp;54 Guelph Avenue Unit 5 Cambridge, ON
+        </a>
+        <div class="bottom--right">
+            <!-- begin bootstrap nav -->
+            <ul class="nav nav-tabs">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                       aria-haspopup="true"
+                       aria-expanded="false">Home</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                       aria-haspopup="true"
+                       aria-expanded="false">About</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                       aria-haspopup="true"
+                       aria-expanded="false">Schedule</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                       aria-haspopup="true"
+                       aria-expanded="false">Join Us</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                       aria-haspopup="true"
+                       aria-expanded="false">Contact</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                        <a class="dropdown-item" href="#">Dropdown</a>
+                    </div>
+                </li>
+            </ul>
+            <!-- end bootstrap nav -->
+        </div>
+    </div>
+</nav>
