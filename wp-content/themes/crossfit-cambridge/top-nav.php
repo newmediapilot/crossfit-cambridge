fc98f42556bc257c61a7ad93615a424cf84fd321
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-08-22 02:46:36 -0400

    working with top nav to make it mobile and desktop friendly

--- wp-content/themes/crossfit-cambridge/top-nav.php
+++ wp-content/themes/crossfit-cambridge/top-nav.php
@@ -1,10 +1,10 @@
 <nav class="npm-top-nav">
     <div class="nmp-top-nav--top">
-        <div class="top--left">
-            Top left
-        </div>
+        <a href="/" class="top--left">
+            <img src="/wp-content/uploads/2019/08/CCLogo_BW_Main-300x266.png" alt="Crossfit Cambridge Logo">
+        </a>
         <div class="top--right">
-            Top right
+            <a href="#" class="btn button-join btn-lg">Join Today</a>
         </div>
     </div>
     <div class="nmp-top-nav--bottom">
@@ -15,58 +15,76 @@
         </a>
         <div class="bottom--right">
             <!-- begin bootstrap nav -->
-            <ul class="nav nav-tabs">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                       aria-haspopup="true"
-                       aria-expanded="false">Home</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                       aria-haspopup="true"
-                       aria-expanded="false">About</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                       aria-haspopup="true"
-                       aria-expanded="false">Schedule</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                       aria-haspopup="true"
-                       aria-expanded="false">Join Us</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                       aria-haspopup="true"
-                       aria-expanded="false">Contact</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                        <a class="dropdown-item" href="#">Dropdown</a>
-                    </div>
-                </li>
-            </ul>
+            <!-- begin bootstrap nav -->
+            <!-- begin bootstrap nav -->
+            <nav class="navbar navbar-expand-lg">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarTargetContent" aria-controls="navbarTargetContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTargetContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Home
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Action</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                About
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Action</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Schedule
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Action</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Join Us
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Action</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Contact
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Action</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- end bootstrap nav -->
+            <!-- end bootstrap nav -->
             <!-- end bootstrap nav -->
         </div>
     </div>
