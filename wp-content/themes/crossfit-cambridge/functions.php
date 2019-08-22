72b62bef049cfe70d167dd0ab16102af02beb121
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-08-21 21:49:22 -0400

    rename keys, install bootstrap, fontawesome

--- wp-content/themes/crossfit-cambridge/functions.php
+++ wp-content/themes/crossfit-cambridge/functions.php
@@ -2,14 +2,23 @@
 
 function load_stylesheets()
 {
-    wp_register_style('killarney-styles', get_template_directory_uri() . '/dist/all.css');
-    wp_enqueue_style('killarney-styles');
+    wp_register_style('framework-styles', get_template_directory_uri() . '/dist/all.css');
+    wp_enqueue_style('framework-styles');
 }
 
 function load_javascripts()
 {
-    wp_register_script('killarney-javascript', get_template_directory_uri() . '/dist/all.js', '', 1, true);
-    wp_enqueue_script('killarney-javascript');
+    wp_register_script('framework-javascript', get_template_directory_uri() . '/dist/all.js', '', 1, true);
+    wp_enqueue_script('framework-javascript');
+
+    wp_register_script('popper-javascript', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', '', 1, true);
+    wp_enqueue_script('popper-javascript');
+
+    wp_register_script('bootstrap-javascript', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', '', 1, true);
+    wp_enqueue_script('bootstrap-javascript');
+
+    wp_register_script('fontawesome-javascript', 'https://kit.fontawesome.com/e696fc9d2a.js', '', 1, true);
+    wp_enqueue_script('fontawesome-javascript');
 }
 
 function add_meta_tags()
@@ -31,7 +40,7 @@ add_action('wp_enqueue_scripts', 'load_javascripts');
 
 add_theme_support('menus');
 
-add_filter( 'widget_text', 'do_shortcode' );
+add_filter('widget_text', 'do_shortcode');
 
 register_nav_menus(
     array(
@@ -72,24 +81,27 @@ function wp_get_page_and_children($slug)
 /**
  * Disable the emoji's
  */
-function disable_emojis() {
-    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-    remove_action( 'wp_print_styles', 'print_emoji_styles' );
-    remove_action( 'admin_print_styles', 'print_emoji_styles' );
-    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+function disable_emojis()
+{
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+    add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
 }
-add_action( 'init', 'disable_emojis' );
+
+add_action('init', 'disable_emojis');
 
 /**
  * Filter function used to remove the tinymce emoji plugin
  */
-function disable_emojis_tinymce( $plugins ) {
-    if ( is_array( $plugins ) ) {
-        return array_diff( $plugins, array( 'wpemoji' ) );
+function disable_emojis_tinymce($plugins)
+{
+    if (is_array($plugins)) {
+        return array_diff($plugins, array('wpemoji'));
     } else {
         return array();
     }
