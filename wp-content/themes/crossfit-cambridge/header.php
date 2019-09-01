eba6539fcfbd5d95e57a74918a73c3477840d70f
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-09-01 01:53:57 -0400

    add analytics

--- wp-content/themes/crossfit-cambridge/header.php
+++ wp-content/themes/crossfit-cambridge/header.php
@@ -4,6 +4,20 @@
 
 <html lang="en">
 <head>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PVY2NL962E"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        gtag('config', 'G-PVY2NL962E');
+    </script>
+
     <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
     <?PHP wp_head(); ?>
 </head>
