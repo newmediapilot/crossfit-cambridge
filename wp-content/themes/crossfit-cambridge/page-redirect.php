16a011bf232bce012279ff424766b89d022807e6
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-09-30 08:29:21 -0400

    fix redirect structure

--- wp-content/themes/crossfit-cambridge/page-redirect.php
+++ wp-content/themes/crossfit-cambridge/page-redirect.php
@@ -1,6 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
 Redirecting please wait...
 <script>
-    (setTimeout(function(){
+    (setTimeout(function () {
         window.location.replace("https://crossfit-cambridge.squarespace.com");
-    },3000)();
+    }, 3000)();
 </script>
+</body>
+</html>
