7dd5cf3cb162ee322a23151926ac48e7972ac045
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-10-08 18:33:14 -0400

    add external tracking for ga events

--- wp-content/themes/crossfit-cambridge/footer.php
+++ wp-content/themes/crossfit-cambridge/footer.php
@@ -6,5 +6,30 @@
 
 <?php wp_footer(); ?>
 
+<script>
+    /**
+     * Function to capture outbound clicks
+     */
+    (function ($) {
+        $('a').click(function () {
+            var $link = $(this);
+            var $curl = $link.attr('href');
+            /**
+             * zenplanner link
+             */
+            if ($curl.indexOf('crossfitcambridge.zenplanner.com')) {
+                ga('send', 'event', 'zenplanner', 'click', $curl);
+            }
+            /**
+             * mailto link
+             */
+            if ($curl.indexOf('mailto:')) {
+                ga('send', 'event', 'mailto', 'click', $curl);
+            }
+        });
+        console.log('activate ga tracking..');
+    })(jQuery);
+</script>
+
 </html>
 <!-- end footer -->
