15df9463bf31610a91386b2a0d6814ce0b04727c
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-12-27 15:33:00 -0500

    update readme and add auto updater

--- .well-known/run-renew.sh
+++ .well-known/run-renew.sh
@@ -1,16 +1,12 @@
-# Renew SSL
-
-## Derived from and modified slightly
-https://lightsail.aws.amazon.com/ls/docs/en_us/articles/amazon-lightsail-using-lets-encrypt-certificates-with-wordpress
-
-### Start verification
-
-```
 DOMAIN=crossfitcambridge.ca
+echo DOMAIN
 sudo certbot -d www.${DOMAIN} -d ${DOMAIN} --webroot -w /home/bitnami/apps/wordpress/htdocs/ --preferred-challenges http certonly
+echo 'running certbot complete...'
 sudo mv /opt/bitnami/apache2/conf/server.key /opt/bitnami/apache2/conf/server.key.old
 sudo mv /opt/bitnami/apache2/conf/server.crt /opt/bitnami/apache2/conf/server.crt.old
+echo 'moving old cert file complete...'
 sudo ln -s /etc/letsencrypt/live/${DOMAIN}/privkey.pem /opt/bitnami/apache2/conf/server.key
 sudo ln -s /etc/letsencrypt/live/${DOMAIN}/fullchain.pem /opt/bitnami/apache2/conf/server.crt
+echo 'linking new cert file complete...'
 sudo /opt/bitnami/ctlscript.sh restart
-```
+echo 'restart complete... ALL DONE!'
